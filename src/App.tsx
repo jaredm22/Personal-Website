f00d39abb44fa8c292e3f189a26c4c2cb4a475d0
Jared Min <jkymin2@gmail.com>
2021-05-08 01:07:04 -0400

    Created spotify app container Sidebar playlist list functional - needs data input navbar card items component created - needs icons
    To-do: Friend activity sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Home from './pages/Home';
+import Spotify from './pages/Spotify';
 import './App.scss';
 
 function App() {
   return (
-    <Home/>
+    <Spotify/>
   );
 }
 
--- src/pages/Spotify.tsx
+++ src/pages/Spotify.tsx
@@ -1,8 +1,28 @@
-import Sidebar from '../components/Sidebar';
 import React from 'react';
+import Conversation from '../components/Conversation';
+import './spotify.scss';
+
+function NavCard(props: any) {
+    return(
+        <div className="nav-card">
+            <div className="icon">
+                
+            </div>
+            <h5>Home</h5>
+        </div>
+    )
+}
+
+function NavBar(props: any) {
+    return(
+        <div className="">
+
+        </div>
+    )
+}
 
 
-export default function Home(props: any) {
+export default function Spotify(props: any) {
     const [ selected, setSelected ] = React.useState(null);
 
     function handleChildClick(childData: any) {
@@ -10,10 +30,60 @@ export default function Home(props: any) {
     }
 
     console.log(selected);
+    const examples = [];
+    for (var i = 0; i < 60; i++) {
+        examples.push(i);
+    }
 
     return(
-        <div className="container">
-            <Sidebar onChildClick={handleChildClick} selectedIndex={selected}/>
+        <div className="spotify-container">
+            <div className="left-sidebar">
+
+                <div className="header">
+                    <div className="button-container">
+                        <div className="button close"></div>
+                        <div className="button minimize"></div>
+                        <div className="button expand"></div>
+                    </div>
+                </div>
+
+               
+
+                <div className="navbar"> 
+                    <NavCard/>
+                    <NavCard/>
+                    <NavCard/>
+                </div>
+
+                <div className="playlists"> 
+                    {examples.map(() => {
+                        return(
+                            <div className="playlist">
+                                <p>late night studying</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+            <div className="spotify-main">
+
+            </div>
+
+            <div className="right-sidebar">
+
+            </div>
+        </div>
+    )
+}
+
+
+function Sidebar(props: any) {
+    const [clicked, setClicked] = React.useState(null); 
+
+    return(
+        <div className="spotify-sidebar">
+            
         </div>
     )
 }
