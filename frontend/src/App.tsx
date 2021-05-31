a11e7848718a35bdb3ef5730ac80dc63186d96cd
Jared Min <jkymin2@gmail.com>
2021-05-30 21:35:15 -0400

    Fixed deployment issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Messenger from './pages/iMessage/Messenger';
 import Spotify from './pages/spotify/Spotify';
 import Dock from './pages/components/Dock/Dock';
-import BigSur from './images/bigsur.jpeg';
 import './App.scss';
 import * as dotenv from 'dotenv';
 
